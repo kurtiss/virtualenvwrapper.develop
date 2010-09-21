3d96e3ee4e220aff867dd22e62b872365ab2553d
Kurtiss Hare <kurtiss@gmail.com>
2010-09-20 17:16:28 -0700

    testing, moar\!

--- virtualenvwrapper/develop.sh
+++ virtualenvwrapper/develop.sh
@@ -133,7 +133,7 @@ develop_selfupdate() {
     sudo pip install git+http://github.com/kurtiss/virtualenvwrapper.develop.git\#egg=virtualenvwrapper --upgrade
     if [ -f "~/.profile" ]
     then
-        . ~/.profile
+        source ~/.profile
     fi
 }
 
